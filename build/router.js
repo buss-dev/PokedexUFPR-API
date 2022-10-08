a21b02537a58321d1f9d32ab08317bc33669ddf7
João Buss <joao.buss@exati.com.br>
2022-10-08 16:48:13 -0300

    JVB - Adicionando .gitignore

--- build/router.js
+++ build/router.js
@@ -2,8 +2,8 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.router = void 0;
 const express_1 = require("express");
-const FirstController_1 = require("./app/controller/FirstController");
+const authenticationRoutes_1 = require("./routes/authenticationRoutes");
 const router = (0, express_1.Router)();
 exports.router = router;
 //Routes
-router.get('/', FirstController_1.firstController.home);
+router.use('/', authenticationRoutes_1.router);
